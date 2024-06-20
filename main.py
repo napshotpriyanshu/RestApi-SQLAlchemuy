9f5ddc831fe57b6b5251c2f572dedb072c55001f
napshotpriyanshu <napshotpriyanshu@gmail.com>
2024-06-20 19:13:06 +0530

    fastapi , sqlalchemy & db postges done

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from crud import CRUD
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from db import engine
+from schemas import NoteModel, NoteCreateModel
+from typing import List
+from models import Note
+import uuid
+from http import HTTPStatus
 
 app = FastAPI(
     title="Noted API",
@@ -6,23 +14,45 @@ app = FastAPI(
     docs_url="/"
 )
 
+session = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False
+)
 
-@app.get('/notes')
+db = CRUD()
+
+
+@app.get('/notes', response_model=List[NoteModel])
 async def get_all_notes():
-    pass
+    notes = await db.get_all(session)
+    return notes
 
-@app.post('/notes')
-async def create_note():
-    pass
+@app.post('/notes', status_code=HTTPStatus.CREATED)
+async def create_note(note_data:NoteCreateModel):
+    new_note = Note(
+        id = str(uuid.uuid4()),
+        title = note_data.title,
+        content = note_data.content
+    )
+    note = await db.add(session, new_note)
+
+    return note
 
 @app.get('/note/{note_id}')
 async def get_note_by_id(note_id):
-    pass
+    note = await db.get_by_id(session,note_id)
+    return note
 
 @app.patch('/note/{note_id}')
-async def update_note(note_id):
-    pass
+async def update_note(note_id, data: NoteCreateModel):
+    note = await db.update(session, note_id, data={
+        'title': data.title,
+        'content': data.content
+    })
+    return note
 
-@app.delete('/note/{note_id}')
+@app.delete('/note/{note_id}', status_code=HTTPStatus.NO_CONTENT)
 async def delete_note(note_id):
-    pass
+    note= await db.get_by_id(session, note_id)
+    result = await db.delete(session, note)
+    return result
